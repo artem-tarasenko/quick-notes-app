695c47378354a9941283a2c5974887dff332929c
Artem Tarasenko <artem.t.88@ya.ru>
2021-08-24 21:00:29 +1000

    setting up sass

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Note.module.scss";
+
+
+
+
+export default function Note(props) {
+
+    return <React.Fragment>
+        <p className={styles.text}>Note component</p>
+    </React.Fragment>
+}
